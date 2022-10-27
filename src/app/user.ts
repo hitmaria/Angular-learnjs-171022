1a3d645fd2dfc09825d34ddc7d0408ff7181152c
hitmaria <hitmariia@gmail.com>
2022-10-28 00:38:36 +0300

    feat: del  rubbish

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,28 +1,3 @@
 export interface IUser {
 	name: string;
 }
-
-var flag1 = 5;
-var flag2 = 5 - flag1;
-if (flag1 - flag2 == 0) {
-	alert('true');
-}
-var exObject = { asd: 1, bnm: 3 };
-
-function exFunc(foo: number, bar: number, baz: number) {
-	switch (foo) {
-		case 1:
-			return 'f';
-		case 2:
-			return 'o';
-		default:
-			return baz;
-	}
-
-	return bar;
-}
-
-if (exObject.asd + exObject.bnm != 10) {
-	var objL = exFunc(1, 2, 3);
-	alert(objL);
-}
