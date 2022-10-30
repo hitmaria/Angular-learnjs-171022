d81040c8ad57ab7d0e77bd86d2854138c438f518
hitmaria <hitmariia@gmail.com>
2022-10-30 20:15:52 +0300

    feat: product-card content

--- src/app/modules/products-list/product-card/product-card.component.ts
+++ src/app/modules/products-list/product-card/product-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { productMock } from '../../../shared/products/product.mock';
 
 @Component({
 	selector: 'app-product-card',
@@ -9,4 +10,46 @@ export class ProductCardComponent implements OnInit {
 	constructor() {}
 
 	ngOnInit(): void {}
+
+	readonly productTitle = productMock.name;
+	readonly productView = productMock.images[0];
+	readonly productImageHeightPx = '300';
+
+	initialFavotiteData = {
+		icon: 'favorite_outline',
+		value: false,
+	};
+
+	productCount = 0;
+	shopBagBadgeVisibility = 'true';
+
+	onFavoriteBtnClick(event: Event) {
+		if (!this.initialFavotiteData.value) {
+			this.initialFavotiteData.value = true;
+			this.initialFavotiteData.icon = 'favorite';
+		} else {
+			this.initialFavotiteData.value = false;
+			this.initialFavotiteData.icon = 'favorite_outline';
+		}
+		event.stopPropagation();
+	}
+
+	onShopigBagClick(event: Event) {
+		this.productCount++;
+		this.changeShopBagBageVisibility();
+		console.log('Buy');
+		event.stopPropagation();
+	}
+
+	changeShopBagBageVisibility() {
+		if (this.productCount == 0) {
+			this.shopBagBadgeVisibility = 'true';
+		} else {
+			this.shopBagBadgeVisibility = 'false';
+		}
+	}
+
+	get productCost(): string {
+		return `${productMock.price} $`;
+	}
 }
--- src/app/modules/products-list/product-card/product-card.module.ts
+++ src/app/modules/products-list/product-card/product-card.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ProductCardComponent } from './product-card.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
 
 @NgModule({
 	declarations: [ProductCardComponent],
-	imports: [CommonModule, MatCardModule, MatButtonModule],
+	imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule, MatBadgeModule],
 	exports: [ProductCardComponent],
 })
 export class ProductCardModule {}
